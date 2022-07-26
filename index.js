aa6f7eb3896e362acf2c6e96abd9285761688b81
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-07-26 15:58:38 -0400

    swapped mysql2 for mysql

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const inquirer = require('inquirer');
+const mysql = require('mySQL');
 
 inquirer
-  .prompt([
-    {
-        type: 'list',
-        name: 'viewOptions',
-        message: 'Choose from the following options:',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee'],
-    },
-  ])
-  .then(answers => {
-    const {choices} = answers;
+    .prompt([
+        {
+            type: 'list',
+            name: 'viewOptions',
+            message: 'Choose from the following options:',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee'],
+        },
+    ])
+    .then(answers => {
+        const { choices } = answers;
 
-    if (choices == 'View All Departments') {
-        viewDepartments();
-    }    
-  });
+        if (choices == 'View All Departments') {
+            viewDepartments();
+        } else if (choices == 'View All Roles') {
+            viewRoles();
+        }
+    });
 
-  const viewDepartments = () => {
-    let sql = SELECT department.name AS department;
-  };
+const viewDepartments = () => {
+    const sql = "SELECT department.id AS id, department.name AS department";
+
+    console.log(sql);
+    return;
+};
